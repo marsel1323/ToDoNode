e70b74188cc081c3a522c26c75009a4e0c7d18bf
marsel1323 <amn132313@gmail.com>
2019-03-08 19:47:05 +0300

    add isAuth middleware

---
+++ src/middlewares/policies/isAuthenticated.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+
+module.exports = async (req, res, next) => {
+  if (process.env.NODE_ENV !== 'Production'
+    && req.header['user-agent'].includes('Postman')) {
+    await next();
+    return;
+  }
+  await passport.authenticate('jwt', async (err, user) => {
+    if (user) {
+      req.user = user;
+      await next();
+    } else {
+      res.status(401).send({ error: 'You do not have access to this resource' });
+    }
+  })(req, next);
+};
